f078859a13739e2d41b1e952e1f577a5a6ab06e8
houssam chakir <thisischchannel@gmail.com>
2024-11-02 21:40:22 +0100

    adding items to list functionality

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+  ]);
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList items={initialItems} />
+      <Form setItems={setItems} items={items} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -18,17 +23,26 @@ export default function App() {
 function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
-function Form() {
+function Form(props) {
+  const {items, setItems} = props
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("description: ", description);
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    setItems([...items, newItem])
+
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😁</h3>
-      <select name='' id=''>
+      <select name='' id='' value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, index) => index + 1).map((i) => {
           return (
             <option key={i} value={i}>
@@ -37,10 +51,7 @@ function Form() {
           );
         })}
       </select>
-      <input
-          type='text' placeholder='Items...'
-          value={description}
-          onChange={(e) => setDescription(e.target.value)} />
+      <input type='text' placeholder='Items...' value={description} onChange={(e) => setDescription(e.target.value)} />
       <button>Add</button>
     </form>
   );
