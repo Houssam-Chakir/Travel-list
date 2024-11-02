22191353589924690561e9b9aa1168f963dc71f0
houssam chakir <thisischchannel@gmail.com>
2024-11-02 20:02:02 +0100

    controlled elements: tracking the stat of the form input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
-    <div className="app">
+    <div className='app'>
       <Logo />
       <Form />
-      <PackingList />
+      <PackingList items={initialItems} />
       <Stats />
     </div>
   );
@@ -12,18 +19,59 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("description: ", description);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your trip 😁</h3>
+      <select name='' id=''>
+        {Array.from({ length: 20 }, (_, index) => index + 1).map((i) => {
+          return (
+            <option key={i} value={i}>
+              {i}
+            </option>
+          );
+        })}
+      </select>
+      <input
+          type='text' placeholder='Items...'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)} />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList(props) {
+  const { items } = props;
+  return (
+    <div className='list'>
+      <ul>
+        {items.map((item, itemIndex) => {
+          return <Item item={item} index={itemIndex} />;
+        })}
+      </ul>
     </div>
   );
 }
-function PackingList() {
-  return <div className='list'>LIST</div>;
+
+function Item(props) {
+  const { item, index } = props;
+  return (
+    <li key={index}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 function Stats() {
   return (
-    <footer>
+    <footer className='stats'>
       <em> You have x items on your list, and you already packed x (x%9)</em>
     </footer>
   );
